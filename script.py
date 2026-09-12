bbce6b273e3165d8371bd1d1e9bd8ee48308f9a8
annahsantos82-creator <annahsantos82@gmail.com>
2026-09-12 01:15:15 +0000

    Adicionar script inicial com exemplos de impressão e condições

---
+++ script.py
@@ -0,0 +1,39 @@
+print("Hello, world!")
+print("olá mundo.")
+print(5+3)
+print( 5+3==7)
+print(5+3==8)
+nome1 = "Ana"
+nome2 = "flavia"
+print (nome1)
+
+nome = "Ana"
+idade = 18
+altura = 1,65
+aprovado = True
+
+print (nome, idade, altura, aprovado)
+
+if nome == "Ana":
+ print ("Olá, Ana")
+
+else: 
+ print ("olá visitante")
+
+if idade >= 18:
+    print ("Voce é maior de idade")
+else: 
+    ("Voce é menor de idade")
+
+#str significa string
+
+print(nome + " tem " + str (idade) + " anos " +
+ str(altura) + " metros de altura e esta aprovado " + str(aprovado) + ".")
+
+
+print(
+   f"{nome} tem {idade}",
+   f"({'é maior de idade' if idade >18 else 'é menor de idade'})"
+   f"mede " {altura} ('é alto') if altura >1,70 else 'não é alto'})
+
+
